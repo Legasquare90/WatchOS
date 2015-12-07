2ce61eab251f01272c60f4714042928709043d35
Jose Angel Cuadrado <legasquare90@gmail.com>
2015-12-07 20:46:21 +0100

    Añadida interfaz de la primera vista: botones. Creado interfaz de la segunda vista. Navegación por segue. Código de WKAlertController. Título primera interfaz. Clean code.

---
+++ Exercise1/Exercise1 WatchKit Extension/HomeInterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeInterfaceController.swift
+//  Exercise1 WatchKit Extension
+//
+//  Created by Jose Angel Cuadrado on 7/12/15.
+//  Copyright © 2015 UPSA. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class HomeInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        self.setTitle("Ejercicio 1")
+    }
+
+    @IBAction func startButtonAction() {
+        let action1 = WKAlertAction(title: "OK", style: .Default, handler:{print("OK")})
+        let action2 = WKAlertAction(title: "Os odio", style: .Destructive) {print("Nos odian :(")}
+        let action3 = WKAlertAction(title: "Cancel", style: .Cancel) {}
+        
+        presentAlertControllerWithTitle("No disponible", message: "", preferredStyle: .ActionSheet, actions: [action1, action2,action3])
+    }
+}
