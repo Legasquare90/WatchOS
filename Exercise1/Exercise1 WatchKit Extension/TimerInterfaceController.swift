2299e47d28de30bbc98e0b812df5150e607b8b9d
Jose Angel Cuadrado <legasquare90@gmail.com>
2015-12-08 20:58:33 +0100

    Código del timer, controlado por un botón, y que cuando llega a 0, cambia el mensaje del botón y la imagen

--- Exercise1/Exercise1 WatchKit Extension/TimerInterfaceController.swift
+++ Exercise1/Exercise1 WatchKit Extension/TimerInterfaceController.swift
@@ -12,20 +12,72 @@ import Foundation
 
 class TimerInterfaceController: WKInterfaceController {
 
+    @IBOutlet var timer: WKInterfaceTimer!
+    @IBOutlet var image: WKInterfaceImage!
+    @IBOutlet var startButton: WKInterfaceButton!
+    
+    var doneTimer: NSTimer?
+    var durationCounter: Int?
+    var duration: Int?
+    var stopped = true
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+        setTitle("Timer")
+        
+        startButton.setTitle("Start")
+        image.setImageNamed("wait")
+        timer.setHidden(true)
+        
+        duration = 30
+        durationCounter = duration
+        doneTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("handleTimer:"), userInfo: nil, repeats: true)
     }
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        
+        timer.stop()
+        durationCounter = 0
+        doneTimer?.invalidate()
+        doneTimer = nil
+        stopped = true
     }
 
+    @IBAction func startButtonAction() {
+        
+        if (stopped) {
+            startButton.setTitle("Stop")
+            stopped = false
+            timer.setHidden(false)
+            timer.setDate(NSDate(timeIntervalSinceNow: NSTimeInterval(durationCounter!)))
+            timer.start()
+        } else {
+            startButton.setTitle("Start")
+            stopped = true
+            timer.stop()
+        }
+        
+    }
+    
+    func handleTimer(timer: NSTimer) {
+        
+        if (!stopped) {
+            
+            durationCounter!--
+            
+            if (durationCounter <= 0) {
+                self.timer.setHidden(true)
+                self.timer.stop()
+                image.setImageNamed("tick")
+                startButton.setTitle("Time's up!")
+                startButton.setEnabled(false)
+                durationCounter = duration
+                stopped = true
+            }
+        }
+        
+    }
+    
 }
