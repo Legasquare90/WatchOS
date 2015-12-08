46791551395ec480ef78b11b5155dbecb7c65323
Jose Angel Cuadrado <legasquare90@gmail.com>
2015-12-08 13:16:41 +0100

    Añadidos ID a interfaces, incluida celda personalizada. Creado código celda personalizada. Creado código de la interfaz de la tabla. Tabla iniciada con número celdas y configuración de la celda (modelo "hardcodeado").

---
+++ Exercise1/Exercise1 WatchKit Extension/RowController.swift
@@ -0,0 +1,15 @@
+//
+//  RowController.swift
+//  Exercise1
+//
+//  Created by Jose Angel Cuadrado on 8/12/15.
+//  Copyright © 2015 UPSA. All rights reserved.
+//
+
+import WatchKit
+
+class RowController: NSObject {
+
+    @IBOutlet var textLabel: WKInterfaceLabel!
+    
+}
---
+++ Exercise1/Exercise1 WatchKit Extension/TableInterfaceController.swift
@@ -0,0 +1,38 @@
+//
+//  TableInterfaceController.swift
+//  Exercise1
+//
+//  Created by Jose Angel Cuadrado on 7/12/15.
+//  Copyright © 2015 UPSA. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class TableInterfaceController: WKInterfaceController {
+
+    @IBOutlet var table: WKInterfaceTable!
+    
+    let model = [0:"Formato", 1:"Timer"]
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        setTitle("Opciones")
+        setupTable()
+    }
+    
+    func setupTable() {
+        table.setNumberOfRows(model.count, withRowType: "rowTable")
+        
+        for(var i:Int = 0; i<model.count; i++) {
+            let row = table.rowControllerAtIndex(i) as! RowController
+            row.textLabel.setText(model[i])
+        }
+    }
+    
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        
+    }
+
+}
