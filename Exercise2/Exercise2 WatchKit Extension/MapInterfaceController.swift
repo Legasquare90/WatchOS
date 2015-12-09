b0ba47d462d00516edbfa08edc1af8d13aea3a04
Jose Angel Cuadrado Mingo <joseangel.cuadrado@intelygenz.com>
2015-12-09 10:06:30 +0100

    Añadidos detalles de la interfaz. Creado código base de la interfaz. Creada navegación "pager" y datos para los mapas.

---
+++ Exercise2/Exercise2 WatchKit Extension/MapInterfaceController.swift
@@ -0,0 +1,23 @@
+//
+//  MapInterfaceController.swift
+//  Exercise2 WatchKit Extension
+//
+//  Created by Jose Angel Cuadrado on 8/12/15.
+//  Copyright © 2015 UPSA. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class MapInterfaceController: WKInterfaceController {
+
+    @IBOutlet var lblPlace: WKInterfaceLabel!
+    @IBOutlet var map: WKInterfaceMap!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+    }
+
+}
