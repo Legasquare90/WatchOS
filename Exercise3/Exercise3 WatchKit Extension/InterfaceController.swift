a1b72ea75926585ab3a876be850b95dbe5db5c08
Jose Angel Cuadrado Mingo <joseangel.cuadrado@intelygenz.com>
2015-12-09 11:10:47 +0100

    Init Exercise 3

---
+++ Exercise3/Exercise3 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Exercise3 WatchKit Extension
+//
+//  Created by Jose Angel  Cuadrado Mingo on 9/12/15.
+//  Copyright © 2015 UPSA. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
