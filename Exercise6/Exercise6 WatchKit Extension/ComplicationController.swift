12354052d6cf542977db22d192542f0aba180363
Jose Angel Cuadrado Mingo <joseangel.cuadrado@intelygenz.com>
2015-12-28 09:42:12 +0100

    Rellenar datos.

--- Exercise6/Exercise6 WatchKit Extension/ComplicationController.swift
+++ Exercise6/Exercise6 WatchKit Extension/ComplicationController.swift
@@ -8,6 +8,22 @@
 
 import ClockKit
 
+struct Show {
+    var name: String
+    var shortName: String?
+    var genre: String
+    
+    var startDate: NSDate
+    var length: NSTimeInterval
+}
+
+let hour: NSTimeInterval = 60 * 60
+let shows = [
+    Show(name: "Into the Wild", shortName: "Into Wild", genre: "Documentary", startDate: NSDate(), length: hour * 1.5),
+    Show(name: "24/7", shortName: nil, genre: "Drama", startDate: NSDate(timeIntervalSinceNow: hour * 1.5), length: hour),
+    Show(name: "How to become rich", shortName: "Become Rich", genre: "Documentary", startDate: NSDate(timeIntervalSinceNow: hour * 2.5), length: hour * 3),
+    Show(name: "NET Daily", shortName: nil, genre: "News", startDate: NSDate(timeIntervalSinceNow: hour * 5.5), length: hour)
+]
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
@@ -32,8 +48,15 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntryForComplication(complication: CLKComplication, withHandler handler: ((CLKComplicationTimelineEntry?) -> Void)) {
-        // Call the handler with the current timeline entry
-        handler(nil)
+        let show = shows[0]
+        let template = CLKComplicationTemplateModularLargeStandardBody()
+        
+        template.headerTextProvider = CLKTimeIntervalTextProvider(startDate: show.startDate, endDate: NSDate(timeInterval: show.length, sinceDate: show.startDate))
+        template.body1TextProvider = CLKSimpleTextProvider(text: show.name, shortText: show.shortName)
+        template.body2TextProvider = CLKSimpleTextProvider(text: show.genre, shortText: nil)
+        
+        let entry = CLKComplicationTimelineEntry(date: NSDate(timeInterval: hour * -0.25, sinceDate: show.startDate), complicationTemplate: template)
+        handler(entry)
     }
     
     func getTimelineEntriesForComplication(complication: CLKComplication, beforeDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
@@ -42,8 +65,24 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     func getTimelineEntriesForComplication(complication: CLKComplication, afterDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
-        // Call the handler with the timeline entries after to the given date
-        handler(nil)
+        var entries: [CLKComplicationTimelineEntry] = []
+        
+        for show in shows
+        {
+            if entries.count < limit && show.startDate.timeIntervalSinceDate(date) > 0
+            {
+                let template = CLKComplicationTemplateModularLargeStandardBody()
+                
+                template.headerTextProvider = CLKTimeIntervalTextProvider(startDate: show.startDate, endDate: NSDate(timeInterval: show.length, sinceDate: show.startDate))
+                template.body1TextProvider = CLKSimpleTextProvider(text: show.name, shortText: show.shortName)
+                template.body2TextProvider = CLKSimpleTextProvider(text: show.genre, shortText: nil)
+                
+                let entry = CLKComplicationTimelineEntry(date: NSDate(timeInterval: hour * -0.25, sinceDate: show.startDate), complicationTemplate: template)
+                entries.append(entry)
+            }
+        }
+        
+        handler(entries)
     }
     
     // MARK: - Update Scheduling
