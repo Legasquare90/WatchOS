16efa656ef303f2e2acc234f478e4717742efea7
Jose Angel Cuadrado <legasquare90@gmail.com>
2016-01-08 17:21:55 +0100

    Init Exercise 10

---
+++ Exercise10/Exercise10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise10
+//
+//  Created by Jose Angel Cuadrado on 8/1/16.
+//  Copyright © 2016 UPSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Exercise10/Exercise10Tests/Exercise10Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Exercise10Tests.swift
+//  Exercise10Tests
+//
+//  Created by Jose Angel Cuadrado on 8/1/16.
+//  Copyright © 2016 UPSA. All rights reserved.
+//
+
+import XCTest
+@testable import Exercise10
+
+class Exercise10Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
