8021f651cff3f8fd9e2a1b9c6a7c3325ed2cea3b
Jose Angel Cuadrado <legasquare90@gmail.com>
2017-01-18 01:24:33 +0100

    Install Alamofire

---
+++ WatchOS 3/HBO/Pods/Target Support Files/Alamofire-watchOS/Alamofire-watchOS-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+#endif
+
+
+FOUNDATION_EXPORT double AlamofireVersionNumber;
+FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
+
---
+++ WatchOS 3/HBO/Pods/Target Support Files/Pods-HBO WatchKit Extension/Pods-HBO WatchKit Extension-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <Foundation/Foundation.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_HBO_WatchKit_ExtensionVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HBO_WatchKit_ExtensionVersionString[];
+
