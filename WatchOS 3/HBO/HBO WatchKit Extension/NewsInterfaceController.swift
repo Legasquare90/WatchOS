d361181bfc3c778c91494d9b9ada94163670e489
Jose Angel Cuadrado <legasquare90@gmail.com>
2017-01-19 00:23:22 +0100

    Show data in news interface

--- WatchOS 3/HBO/HBO WatchKit Extension/NewsInterfaceController.swift
+++ WatchOS 3/HBO/HBO WatchKit Extension/NewsInterfaceController.swift
@@ -10,9 +10,16 @@ import WatchKit
 import Foundation
 import Alamofire
 
-
 class NewsInterfaceController: WKInterfaceController {
 
+    enum NewsType: Int {
+        case bigNews = 1, smallNews = 2
+    }
+    
+    var news: [[String: Any]] = []
+    
+    @IBOutlet var table: WKInterfaceTable!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         setTitle("Noticias")
@@ -29,7 +36,53 @@ class NewsInterfaceController: WKInterfaceController {
             }
             
             print(newsList)
+            self.news = newsList
+            self.setupTable()
         }
     }
+    
+    func setupTable() {
+        let countBigNews = news.filter { (newsItem) -> Bool in
+            let type = newsItem["newsType"] as! Int
+            return type == NewsType.bigNews.rawValue
+        }.count
+        var rowTypes = Array(repeating: "RowBigNews", count: countBigNews)
+        rowTypes += Array(repeating: "RowSmallNews", count: news.count - countBigNews)
+        table.setRowTypes(rowTypes)
+        for i in 0 ..< news.count {
+            let newsItem = news[i] as [String: Any]
+            if ((newsItem["newsType"] as! Int) == NewsType.bigNews.rawValue) {
+                let row = table.rowController(at: i) as! RowBigNewsController
+                let title = newsItem["title"] as! String
+                row.newsTitle.setText(title)
+                let urlImage = URL(string: (newsItem["image"] as? String)!)
+                let data = try? Data(contentsOf: urlImage!)
+                let image = UIImage(data: data!)
+                row.newsImage.setImage(image)
+            } else {
+                let row = table.rowController(at: i) as! RowSmallNewsController
+                let title = newsItem["title"] as! String
+                row.newsTitle.setText(title)
+                let urlImage = URL(string: (newsItem["image"] as? String)!)
+                let data = try? Data(contentsOf: urlImage!)
+                let image = UIImage(data: data!)
+                row.newsImage.setImage(image)
+            }
+        }
+    }
+
+}
+
+class RowBigNewsController: NSObject {
+    
+    @IBOutlet var newsImage: WKInterfaceImage!
+    @IBOutlet var newsTitle: WKInterfaceLabel!
+    
+}
+
+class RowSmallNewsController: NSObject {
+    
+    @IBOutlet var newsImage: WKInterfaceImage!
+    @IBOutlet var newsTitle: WKInterfaceLabel!
 
 }
