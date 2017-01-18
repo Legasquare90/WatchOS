c183900e8f09f309d6d24b4f71beb115445f7771
Jose Angel Cuadrado <legasquare90@gmail.com>
2017-01-18 01:14:02 +0100

    Create controllers

--- WatchOS 3/HBO/HBO WatchKit Extension/NewsInterfaceController.swift
+++ WatchOS 3/HBO/HBO WatchKit Extension/NewsInterfaceController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  NewsInterfaceController.swift
 //  HBO WatchKit Extension
 //
 //  Created by Jose Angel Cuadrado on 17/1/17.
@@ -10,7 +10,7 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class NewsInterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
